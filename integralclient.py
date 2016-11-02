af752529f6c2d20ce54baca519e75a5141e73f7a
Volodymyr Savchenko <vladimir.savchenko@gmail.com>
2016-11-02 09:43:53 +0000

    error handling in converttime

--- integralclient.py
+++ integralclient.py
@@ -25,6 +25,9 @@ def converttime(informat,intime,outformat):
 
     r=requests.get('http://'+integral_services_server+'/integral/integral-timesystem/api/v1.0/'+informat+'/'+intime+'/'+outformat,auth=auth)
 
+    if r.status_code!=200:
+        raise Exception('error from timesystem server: '+r.content)
+
     if outformat=="ANY":
         try:
             return r.json()
