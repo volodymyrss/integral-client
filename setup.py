28ab6d1f3d3d1988a132c83fdab2b02f4e09b07b
Volodymyr <vladimir.savchenko@gmail.com>
2019-09-02 16:28:52 +0200

    down isgri

--- setup.py
+++ setup.py
@@ -1,19 +1,13 @@
 from setuptools import setup
+import ast
+import sys
+
+setup_requires = ['setuptools >= 30.3.0', 'setuptools-git-version']
+if {'pytest', 'test', 'ptr'}.intersection(sys.argv):
+    setup_requires.append('pytest-runner')
 
 
-setup(
-        name='integral-client',
-        version='1.0.7',
-        py_modules= ['integralclient','ddaclient','service_exception'],
-        url="http://odahub.io",
-        package_data     = {
-            "": [
-                "*.txt",
-                "*.md",
-                "*.rst",
-                "*.py"
-                ]
-            },
-        license='Creative Commons Attribution-Noncommercial-Share Alike license',
-        long_description=open('README.md').read(),
-        )
+setup(description="integral client",
+      long_description=open('README.md').read(),
+      version_format = '{tag}.dev{commitcount}+{gitsha}', 
+      setup_requires=setup_requires)
