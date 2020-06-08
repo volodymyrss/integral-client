7ab59820038ff0656da5f46ce14315f9bd25e9b3
Volodymyr SAVCHENKO <contact@volodymyrsavchenko.com>
2020-06-08 19:05:40 +0200

    renamed and regroupe

--- test.py
+++
@@ -1,7 +0,0 @@
-import integralclient as ic
-from service_exception import *
-
-try:
-    c=ic.query_web_service("integral-dumplc","api/v1.0/ACS/2016-11-23T13:34:37.51/602")
-except ic.Waiting as e:
-    print e.args[1]['key']
