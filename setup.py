86f04105f8a0cda4339cf09af0e1ed8e69106ed0
Volodymyr SAVCHENKO <contact@volodymyrsavchenko.com>
2020-06-11 16:38:59 +0200

    Bump version: 1.1.4-dev0 → 1.1.4

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ if {'pytest', 'test', 'ptr'}.intersection(sys.argv):
 
 setup(description="integral-client",
       long_description=open('README.md').read(),
-      version='1.1.4-dev0',
+      version='1.1.4',
       include_package_data=True,
       setup_requires=setup_requires)
